a242dab04fb48dca6ecd823e43c1f4e3b96c92b4
James McCann <jmccnz@gmail.com>
2015-02-23 11:54:26 +1300

    Added trigger/anim for showing/hiding new goal form on HomeActivity

--- app/src/main/java/co/pennypot/app/HomeActivity.java
+++ app/src/main/java/co/pennypot/app/HomeActivity.java
@@ -1,8 +1,14 @@
 package co.pennypot.app;
 
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
 
@@ -13,26 +19,24 @@ public class HomeActivity extends Activity {
 
     private ListView mGoalsList;
 
+    private boolean mNewGoalFormVisible;
+
+    private RelativeLayout mRootLayout;
+
+    private View mNewGoalForm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        mRootLayout = (RelativeLayout) findViewById(R.id.container);
         initGoalsList();
     }
 
     private void initGoalsList() {
         //TODO: remove static data
         ArrayList<Goal> goalsList = new ArrayList<Goal>();
-        goalsList.add(new Goal("San Fran", 2250, 2500));
-        goalsList.add(new Goal("Macbook Pro", 450, 3200));
-        goalsList.add(new Goal("New Sofa", 900, 1600));
-        goalsList.add(new Goal("San Fran", 2250, 2500));
-        goalsList.add(new Goal("Macbook Pro", 450, 3200));
-        goalsList.add(new Goal("New Sofa", 900, 1600));
-        goalsList.add(new Goal("San Fran", 2250, 2500));
-        goalsList.add(new Goal("Macbook Pro", 450, 3200));
-        goalsList.add(new Goal("New Sofa", 900, 1600));
         goalsList.add(new Goal("San Fran", 2250, 2500));
         goalsList.add(new Goal("Macbook Pro", 450, 3200));
         goalsList.add(new Goal("New Sofa", 900, 1600));
@@ -43,6 +47,42 @@ public class HomeActivity extends Activity {
 
         mGoalsList.setOnTouchListener(touchListener);
         mGoalsList.setOnScrollListener(touchListener.makeScrollListener());
+    }
+
+    public void onNewGoalPressed(View view) {
+        if (!mNewGoalFormVisible) {
+            mNewGoalForm = new NewGoalFormView(this);
+            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            params.addRule(RelativeLayout.ABOVE, R.id.goal_list_container);
+            mNewGoalForm.setLayoutParams(params);
+            mRootLayout.addView(mNewGoalForm, 1);
+
+            mNewGoalForm.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                @Override
+                public void onGlobalLayout() {
+                    mNewGoalForm.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                    float height = mNewGoalForm.getHeight();
+                    ObjectAnimator.ofFloat(mNewGoalForm, "translationY", 0.0f, height).start();
+                }
+            });
+        } else {
+            float height = mNewGoalForm.getHeight();
+            ObjectAnimator anim = ObjectAnimator.ofFloat(mNewGoalForm, "translationY", height, 0.0f);
+            anim.addListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mRootLayout.removeView(mNewGoalForm);
+                }
+
+                public void onAnimationStart(Animator animation) { }
+                public void onAnimationCancel(Animator animation) { }
+                public void onAnimationRepeat(Animator animation) { }
+            });
+            anim.start();
+        }
+
+        mNewGoalFormVisible = !mNewGoalFormVisible;
     }
 
 }
